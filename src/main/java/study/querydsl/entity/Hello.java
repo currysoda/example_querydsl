d7bb84965f92b3740cb36caf65aab4c43126eb10
currysoda <98086265+currysoda@users.noreply.github.com>
2024-08-12 19:36:21 +0900

    first_commit

---
+++ src/main/java/study/querydsl/entity/Hello.java
@@ -0,0 +1,20 @@
+package study.querydsl.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Setter
+@Getter
+public class Hello {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private long id;
+	
+	
+}
---
+++ src/test/java/study/querydsl/entity/MemberTest.java
@@ -0,0 +1,29 @@
+package study.querydsl.entity;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Commit;
+import org.springframework.transaction.annotation.Transactional;
+
+@SpringBootTest
+@Transactional
+@Commit
+class MemberTest {
+	
+	@PersistenceContext
+	EntityManager em;
+	
+	@Test
+	public void testEntity() {
+		Team teamA = new Team("teamA");
+		Team teamB = new Team("teamB");
+		em.persist(teamA);
+		em.persist(teamB);
+		
+		
+	}
+}
